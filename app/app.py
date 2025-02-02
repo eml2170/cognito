50ca95913d4c0a4e83be9b11c4733611a00db68d
Edward Liu <edwardmarkliu@gmail.com>
2025-02-02 04:49:57 -0500

    add logo

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
